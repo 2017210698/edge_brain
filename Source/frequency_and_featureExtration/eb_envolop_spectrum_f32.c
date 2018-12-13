b5eebb37793584c3949561515418ad755cc692ec
lwg17 <lwg17@mails.tsinghua.edu.cn>
2018-12-13 11:37:48 +0800

    12/13, programs can run, the result is not perfect!

--- Source/frequency_and_featureExtration/eb_envolop_spectrum_f32.c
+++ Source/frequency_and_featureExtration/eb_envolop_spectrum_f32.c
@@ -33,39 +33,23 @@ void eb_envolop_spectrum_f32(
 	uint16_t srcLen,
 	uint16_t fftSize)
 {
-//	uint8_t ifftFlag=0; // rfft if flag is 0, rifft if flag is 1
-//	arm_rfft_fast_instance_f32 S;
-//		/* initiate the structure */
-//	arm_rfft_fast_init_f32(&S,fftSize);
-//	
-//	/* 1024 points arm_rfft_fast_f32 */
-//	arm_rfft_fast_f32(&S,pSrc,pFftBuffer,ifftFlag);
 
-//	if(srcLen%2!=0)
-//	{
-//		srcLen--;
-//	}
-
-//	
-//	arm_cmplx_mag_f32(pSrc,pSrc_fft,1024);
-//	
-
-	uint16_t i; 
-	arm_rfft_fast_instance_f32 S; 
-	/* 实数序列FFT长度 */ 
-	uint8_t ifftFlag = 0; /* 初始化结构体S中的参数 */ 
-	float32_t testInput_f32_10khz[1024];
-	float32_t testOutput_f32_10khz[2048];
-	float32_t testOutput[1024];
-	arm_rfft_fast_init_f32(&S, fftSize);
+	uint8_t ifftFlag=0; // rfft if flag is 0, rifft if flag is 1
+	//float32_t interValue;
+	float32_t pIfftBuffer[2048];
+	float32_t pInterBuffer[1024];
+	arm_rfft_fast_instance_f32 S1,S2;
+	/* initiate the structure */
+	arm_rfft_fast_init_f32(&S1,fftSize);
+	arm_rfft_fast_init_f32(&S2,2*fftSize);
 	
-	/* 按照实部，虚部，实部，虚部..... 的顺序存储数据 */ 
-	for(i=0; i<1024; i++) 
-	{ /* 50Hz正弦波，采样率1KHz */ 
-	testInput_f32_10khz[i] = 1.2f*arm_sin_f32(2*3.1415926f*50*i/1000)+1; }
-	/* 1024点实序列快速FFT */ 
-	arm_rfft_fast_f32(&S, testInput_f32_10khz, testOutput_f32_10khz, ifftFlag);
-	arm_cmplx_mag_f32(testOutput_f32_10khz, testOutput, fftSize);
-	
-	}
+	/* hilbert transform */
+	arm_rfft_fast_f32(&S1,pSrc,pFftBuffer,ifftFlag);
+	arm_scale_f32(pFftBuffer,2,pFftBuffer,2*fftSize);
+	ifftFlag=1;
+	arm_rfft_fast_f32(&S2,pFftBuffer,pIfftBuffer,ifftFlag);
 
+	arm_cmplx_mag_f32(pIfftBuffer,pInterBuffer,fftSize);
+	arm_rfft_fast_f32(&S1,pInterBuffer,pFftBuffer,ifftFlag);
+	arm_cmplx_mag_f32(pFftBuffer,pDst,fftSize);
+}
